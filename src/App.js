f0789ff0bc13d23bd17f03c84d19f9fd7368c54f
Moutlou <moutlou@outlook.com>
2019-05-02 16:11:40 +0300

    new game button working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React  , {forwardRef, userRef , useImperativeHandle } from 'react';
+import React   from 'react';
 import './App.css';
 
 const buttonEnabledBgColor = '#123456';
@@ -24,11 +24,19 @@ class HangMan extends React.Component {
       mistakes : this.props.mistakes
     }
   }
+  newGame = () => {    
+    this.setState({
+      mistakes : 0
+    })
+  }
+
   increaseMistakes = ()=> {
     this.setState({
       mistakes: this.state.mistakes+1
     })
   }
+  
+
   render() {
 
     return (
@@ -57,41 +65,48 @@ class Letter extends React.Component {
       activeClassName : "btn btn-sm btn-primary" ,
       background : buttonEnabledBgColor ,
       check : this.props.check
-    };
-    
-    this.disable = () => {
-      if(this.state.gameFinished === false) {
-        this.setState ({
-          disabled : true,
-          activeClassName : "btn btn-sm btn-primary disabled",
-          background : buttonDisabledBgColor
-        });
+    };  
+  }
+  newGame = ()=> {
+    this.setState({
+      gameFinished : false ,
+      disabled: false ,
+      activeClassName : "btn btn-sm btn-primary" ,
+      background : buttonEnabledBgColor ,
+      check : this.props.check
+    })
+  }
+  disable = () => {
+    if(this.state.gameFinished === false) {
+      this.setState ({
+        disabled : true,
+        activeClassName : "btn btn-sm btn-primary disabled",
+        background : buttonDisabledBgColor
+      });
+    }
+  }
+  enable = () => {
+    if(this.state.gameFinished === false) {
+      this.setState ({
+        disabled : false,
+        activeClassName : "btn btn-sm btn-primary",
+        background : buttonEnabledBgColor
+      });
+    }
+  }
+  checkLetter = () => {
+    if(this.state.gameFinished === false) {
+      if(this.state.disabled!==true) {        
+        this.disable();
+        this.state.check(this.state.letter);
       }
     }
-    this.enable = () => {
-      if(this.state.gameFinished === false) {
-        this.setState ({
-          disabled : false,
-          activeClassName : "btn btn-sm btn-primary",
-          background : buttonEnabledBgColor
-        });
-      }
-    }
-    this.checkLetter = () => {
-      if(this.state.gameFinished === false) {
-        if(this.state.disabled!==true) {        
-          this.disable();
-          this.state.check(this.state.letter);
-        }
-      }
-    }
-    
   }
   gameFinished = ()=> {
     this.setState({
       gameFinished: true
     })
-  }
+  } 
   render(){
     return(
       <div className={this.state.activeClassName} style={{background:this.state.background}} onClick={evt=>this.checkLetter()}>{this.props.letter}</div>
@@ -103,7 +118,7 @@ class Word extends React.Component {
   constructor(props) {
     super(props);
     let word = this.props.word.toUpperCase();    
-    this.wordLetters = new Array();       
+    this.wordLetters = [];       
     this.state = 
     {
       word : word.split('')      
@@ -112,11 +127,21 @@ class Word extends React.Component {
     for(let i=0; i<word.length;i++) {
       this.wordLetters[i] = React.createRef();             
     }
+  }
+  newGame = () => {    
+    let word = this.props.word.toUpperCase();            
+    this.state = 
+    {
+      word : word.split('')      
+    }    
 
+    for(let i=0; i<word.length;i++) {
+      this.wordLetters[i].current.newGame();            
+    }
   }
   showLetter = (letter) => {
     for(let i=0; i<this.state.word.length;i++) {
-      if(this.wordLetters[i].current.props.letter == letter )
+      if(this.wordLetters[i].current.props.letter === letter )
       {
         this.wordLetters[i].current.showLetter();
       }
@@ -147,7 +172,12 @@ class WordLetter extends React.Component {
       fnShowCharacter : this.showCharacter
     }
   }
-
+  newGame = () => {
+    this.setState({
+      letter : this.props.letter ,
+      finded : false      
+    })
+  }
   showLetter = ()=> {
     this.setState({
       finded:true
@@ -175,7 +205,12 @@ class Panel extends React.Component {
       message : ''      
     }
   }
-
+  newGame = () => {
+    this.setState ({
+      score: 0 ,
+      message : ''      
+    });
+  }
   addScore(score) {
     this.setState({
       score: this.state.score+score
@@ -210,7 +245,7 @@ class Game extends React.Component {
     this.wordComponent = React.createRef();
     this.hangManComponent = React.createRef();
     this.panelComponent = React.createRef();
-    this.lettersComponents = new Array();
+    this.lettersComponents = [];
 
     let WORD = "test";
 
@@ -227,79 +262,101 @@ class Game extends React.Component {
 
     for(let i=0; i<letters.length;i++) {
       this.lettersComponents[i] = React.createRef();             
+    }   
+  }
+
+  newGame = () => {
+    
+    let WORD = "test";
+    this.setState({
+      mistakes : 0,
+      score : 0,
+      word: WORD.toUpperCase(),
+      wordLength : WORD.length,
+      findedLetters : 0 ,
+
+      win : false,
+      lost: false
+    });
+    
+    this.hangManComponent.current.newGame();
+    this.wordComponent.current.newGame();
+    this.panelComponent.current.newGame();
+    for(let i=0;i<letters.length;i++) {
+      this.lettersComponents[i].current.newGame();
     }
 
-    this.howManyTimesExists = (letter) => {
-      if( !this.state.win && !this.state.lost ) {
-        let times = 0;
-        for(let i = 0;i<this.state.word.length;i++) {
-          if(this.state.word[i]===letter) {
-            times++;
-          }
-        }      
-        return times;
-      }
-    }
+  }
 
-    this.checkLetter = (letter)=> {
-      if( !this.state.win && !this.state.lost ) {
-        let times = this.howManyTimesExists(letter); 
-        let addToScore = singleCharScore;
-        if(times > 0 )
-        {              
-          if(times > 1)
-          {
-            addToScore = times * ( singleCharScore * times);
-          } 
-          
-          let newScore = this.state.score+addToScore;
-          let newFindedLetters = this.state.findedLetters + times;
-
-          this.setState({
-            score: newScore ,
-            findedLetters : newFindedLetters
-          });                
-          this.panelComponent.current.addScore(addToScore);
-          this.wordComponent.current.showLetter(letter);    
-          
-          if(this.state.wordLength === newFindedLetters) {
-            this.panelComponent.current.youWin();
-            this.setState({
-              win:true
-            });
-            this.gameFinished();
-          }
+  howManyTimesExists = (letter) => {
+    if( !this.state.win && !this.state.lost ) {
+      let times = 0;
+      for(let i = 0;i<this.state.word.length;i++) {
+        if(this.state.word[i]===letter) {
+          times++;
         }
-        else
+      }      
+      return times;
+    }
+  }
+
+  checkLetter = (letter)=> {
+    if( !this.state.win && !this.state.lost ) {
+      let times = this.howManyTimesExists(letter); 
+      let addToScore = singleCharScore;
+      if(times > 0 )
+      {              
+        if(times > 1)
         {
-          let newMistakes = this.state.mistakes + 1;  
-          this.setState({
-            mistakes: newMistakes
-          })             
-          this.hangManComponent.current.increaseMistakes();
-          if(newMistakes== 7){
-            this.panelComponent.current.youLost();
-            this.setState({
-              lost:true
-            });
-            this.gameFinished();
-          }        
+          addToScore = times * ( singleCharScore * times);
         } 
-      }     
-    }    
+        
+        let newScore = this.state.score+addToScore;
+        let newFindedLetters = this.state.findedLetters + times;
 
-    this.check = (letter) => {
-      if( !this.state.win && !this.state.lost ) {
-        this.checkLetter(letter);
+        this.setState({
+          score: newScore ,
+          findedLetters : newFindedLetters
+        });                
+        this.panelComponent.current.addScore(addToScore);
+        this.wordComponent.current.showLetter(letter);    
+        
+        if(this.state.wordLength === newFindedLetters) {
+          this.panelComponent.current.youWin();
+          this.setState({
+            win:true
+          });
+          this.gameFinished();
+        }
       }
-    }
+      else
+      {
+        let newMistakes = this.state.mistakes + 1;  
+        this.setState({
+          mistakes: newMistakes
+        })             
+        this.hangManComponent.current.increaseMistakes();
+        if(newMistakes === 7){
+          this.panelComponent.current.youLost();
+          this.setState({
+            lost:true
+          });
+          this.gameFinished();
+        }        
+      } 
+    }     
+  }    
 
-    this.gameFinished = () => {
-      for(let i=0;i<letters.length;i++) {
-        this.lettersComponents[i].current.gameFinished();
-      }
+  check = (letter) => {
+    if( !this.state.win && !this.state.lost ) {
+      this.checkLetter(letter);
     }
+  }
 
+  gameFinished = () => {
+    for(let i=0;i<letters.length;i++) {
+      this.lettersComponents[i].current.gameFinished();
+    }
   }
 
   render()
@@ -309,12 +366,12 @@ class Game extends React.Component {
       <div>
       <div className="row">
         <div className="col-md-3 hangmanCol">
-        <HangMan mistakes={this.state.mistakes} ref={this.hangManComponent}/>
+        <HangMan mistakes={this.state.mistakes} ref={this.hangManComponent} />
         </div>
         <div className="col-md-9 panelCol">
           <div className="row">
             <div className="col-md-12">
-              <Button title="New game"/>
+              <div className="btn btn-sm btn-primary" onClick={evt=> this.newGame()}>New game</div>              
             </div>            
           </div>
           <div className="row">&nbsp;</div>
