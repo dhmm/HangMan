3a1ed2d0c268aa02b750505d5fc5253a7bf61e62
Moutlou <moutlou@outlook.com>
2019-05-02 14:51:10 +0300

    find letters ready

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React  , {forwardRef, userRef , useImperativeHandle } from 'react';
 import './App.css';
 
 const buttonEnabledBgColor = '#123456';
@@ -8,10 +8,6 @@ const letters = [
 ];
 
 class Button extends React.Component {
-  constructor(props)
-  {
-    super(props);
-  }
   render()
   {
     return(
@@ -20,18 +16,27 @@ class Button extends React.Component {
   }
 }
 class HangMan extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mistakes : this.props.mistakes
+    }
+  }
   render() {
+
     return (
-      <div className="hangmanContainer">
-        <div className="hangmanHead"></div>
-        <div className="hangmanHeadDown"></div>
-        <div className="hangmanBodyContainer">
-        <div className="hangmanBody"></div>
-          <div className="hangmanLeftArm"></div>
-          <div className="hangmanRightArm"></div>
-        </div>
-        <div className="hangmanLeftLeg"></div>
-        <div className="hangmanRightLeg"></div>            
+      <div className="hangmanContainer">        
+
+          { this.state.mistakes >= 1 ? <div className="hangmanHead"></div> : '' }
+          { this.state.mistakes >= 2 ? <div className="hangmanHeadDown"></div> : '' }  
+          { this.state.mistakes >= 3 ? <div className="hangmanBodyContainer"> 
+              <div className="hangmanBody"></div>
+              { this.state.mistakes >= 4 ? <div className="hangmanLeftArm"></div> : '' }
+              { this.state.mistakes >= 5 ? <div className="hangmanRightArm"></div> : '' }
+          </div>  : '' }                                 
+          { this.state.mistakes >= 6 ? <div className="hangmanLeftLeg"></div>  : '' }
+          { this.state.mistakes >= 7 ? <div className="hangmanRightLeg"></div> : '' }
       </div>
     )
   }
@@ -43,7 +48,8 @@ class Letter extends React.Component {
     this.state = {
       disabled: false ,
       activeClassName : "btn btn-sm btn-primary" ,
-      background : buttonEnabledBgColor
+      background : buttonEnabledBgColor ,
+      fnCheckLetter : this.props.fnCheckLetter
     };
 
     this.disable = () => {
@@ -60,25 +66,86 @@ class Letter extends React.Component {
         background : buttonEnabledBgColor
       });
     }
-    this.changeState = () => {
-      if(this.state.disabled===true) {
-        this.enable();
-      } else {
+    this.checkLetter = () => {
+      if(this.state.disabled!==true) {        
         this.disable();
+        this.state.fnCheckLetter(this.state.letter);
       }
     }
     
   }
   render(){
     return(
-      <div className={this.state.activeClassName} style={{background:this.state.background}} onClick={this.changeState}>{this.props.letter}</div>
+      <div className={this.state.activeClassName} style={{background:this.state.background}} onClick={evt=>this.checkLetter()}>{this.props.letter}</div>
     )
   }
 }
-class WordLetter extends React.Component {
+
+class Word extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    let word = this.props.word.toUpperCase();    
+    this.wordLetters = new Array();       
+    this.state = 
+    {
+      word : word.split('')      
+    }    
+
+    for(let i=0; i<word.length;i++) {
+      this.wordLetters[i] = React.createRef();             
+    }
+
+  }
+  showLetter = (letter) => {
+    for(let i=0; i<this.state.word.length;i++) {
+      if(this.wordLetters[i].current.props.letter == letter )
+      {
+        this.wordLetters[i].current.showLetter();
+      }
+    }    
+  }
+
   render() {
+    let i=0;
+    return(      
+      <div className="wordContainer">
+      {        
+        this.state.word.map( (letter) => {
+          return <WordLetter letter={letter} ref={this.wordLetters[i++]}/>
+        })       
+      }
+      </div>
+    );
+  }
+}
+class WordLetter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = 
+    {
+      letter : this.props.letter ,
+      finded : false ,
+
+      fnShowCharacter : this.showCharacter
+    }
+  }
+
+  showLetter = ()=> {
+    this.setState({
+      finded:true
+    });
+  }
+
+  render() {
+    var displayedLetter = '-';
+    if(this.state.finded===true) {
+      displayedLetter = this.state.letter
+    }
+    
+
     return(
-      <div className="wordLetter">A</div>
+      <div className="wordLetter">{displayedLetter}</div>
     );
   }
 }
@@ -89,10 +156,10 @@ class Panel extends React.Component {
 
     this.state = {
       score: 0 ,
-      message : ''
+      message : ''      
     }
 
-    
+
   }
   render() {
     return (
@@ -103,14 +170,34 @@ class Panel extends React.Component {
     );
   }
 }
-function App() {
-  
-  
-  return (
-    <div>
+class Game extends React.Component {
+  constructor(props)
+  {
+    super(props);
+    this.wordComponent = React.createRef();
+
+    this.state = {
+      mistakes : 0,
+      score : 0,
+      word: 'ALELA' 
+    } 
+    this.checkLetter = (letter)=> {
+      if(this.state.word.indexOf(letter) > -1 ) {          
+        this.wordComponent.current.showLetter(letter);      
+      } else {
+        alert("Problemmmmm");
+      }
+    }
+
+  }
+
+  render()
+  {
+    return(
+      <div>
       <div className="row">
         <div className="col-md-3 hangmanCol">
-        <HangMan/>
+        <HangMan mistakes={this.state.mistakes}/>
         </div>
         <div className="col-md-9 panelCol">
           <div className="row">
@@ -129,25 +216,28 @@ function App() {
       </div>
       <div className="row">
         <div className="col-md-12">
-          <div className="wordContainer">
-          <WordLetter/>
-          <WordLetter/>
-          <WordLetter/>
-          <WordLetter/>
-          </div>
+          {<Word word="ALELA" ref={this.wordComponent}/>}
         </div>
       </div>
       <div className="row">
         <div className="col-md-12">
           { 
             letters.map((letter) => {
-              return <Letter letter={letter}/>
+              return <Letter letter={letter} fnCheckLetter={evt=> this.checkLetter(letter)} />
             })
           }
         </div>        
       </div>            
     </div>
-  );
+    );
+  }
 }
 
+
+
+function App() {    
+  return (
+    <Game/>
+  );
+}
 export default App;
